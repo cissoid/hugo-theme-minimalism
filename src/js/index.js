e21bc85da17ba5f5c4a14d3a483100a8b5df57ba
cissoid <yangtukun1412@gmail.com>
2019-01-24 16:32:29 +0800

    Add, pjax.

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,19 @@
  * File Name: index.js
  * Author: cissoid
  * Created At: 2017-04-19T11:07:14+0800
- * Last Modified: 2019-01-21T16:55:12+0800
+ * Last Modified: 2019-01-24T16:29:19+0800
  */
 
 require('sass/style.scss');
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
+const Pjax = require('pjax');
+
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
+  }
+  new Pjax({
+    selectors: ['title', 'main'],
+    cacheBust: false
   });
-}
+});
