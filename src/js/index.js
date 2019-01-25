21110e78cb889bca6662c2983b783e30440e2c58
cissoid <yangtukun1412@gmail.com>
2019-01-25 11:54:48 +0800

    Add, pjax.

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
  * File Name: index.js
  * Author: cissoid
  * Created At: 2017-04-19T11:07:14+0800
- * Last Modified: 2019-01-24T16:29:19+0800
+ * Last Modified: 2019-01-25T11:52:53+0800
  */
 
 require('sass/style.scss');
@@ -15,6 +15,33 @@ window.addEventListener('load', function() {
   }
   new Pjax({
     selectors: ['title', 'main'],
+    switches: {
+      // 'title': Pjax.switches.sideBySide,
+      'main': function(oldEl, newEl, options, switchOptions) {
+        var _this = this;
+        const animationEndEvents = ['animationend', 'webkitAnimationEnd', 'MSAnimationEnd', 'oanimationend'];
+        new Promise(function(resolve, reject) {
+          oldEl.classList.add('out');
+          animationEndEvents.forEach(function(e) {
+            oldEl.addEventListener(e, resolve, true);
+          })
+        }).then(function() {
+          const parent = oldEl.parentNode;
+          newEl.hide = true;
+          parent.insertBefore(newEl, oldEl);
+          parent.removeChild(oldEl);
+          return new Promise(function(resolve, reject) {
+            newEl.classList.add('in');
+            animationEndEvents.forEach(function(e) {
+              newEl.addEventListener(e, resolve, true);
+            })
+          });
+        }).then(function() {
+          newEl.classList.remove('in');
+          _this.onSwitch();
+        });
+      }
+    },
     cacheBust: false
   });
 });
