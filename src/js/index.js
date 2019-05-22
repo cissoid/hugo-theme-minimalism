c16c0ae25502876379413425ef1b20687b661c98
cissoid <yangtukun1412@gmail.com>
2019-05-22 09:46:41 +0800

    Modify, fix vh problem.

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,24 @@
  * File Name: index.js
  * Author: cissoid
  * Created At: 2017-04-19T11:07:14+0800
- * Last Modified: 2019-02-01T16:28:22+0800
+ * Last Modified: 2019-05-22T09:44:36+0800
  */
 
 require('sass/style.scss');
 
 const Pjax = require('js/pjax');
 
+function vhTrick() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
 window.addEventListener('load', () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
   }
   new Pjax();
+  vhTrick();
 });
+
+window.addEventListener('resize', vhTrick);
